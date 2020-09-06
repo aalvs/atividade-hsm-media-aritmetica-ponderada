e403e3f30645f1b8fc7ab9ffd06dc146956f63e6
Andre Alves <andrealvesdeoliveira@outlook.com>
2020-09-05 21:48:37 -0300

    Alterado os dados autorais do projeto

--- MediaAluno/src/mediaaluno/MediaSemestral.java
+++ MediaAluno/src/mediaaluno/MediaSemestral.java
@@ -1,15 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package mediaaluno;
 
 import java.text.DecimalFormat;
 import java.util.Scanner;
 
 /**
- *
+ * Instituição: HSM University
+ * Diciplina: Lógica de Programação e Orientação a Objetos
+ * Aluno: André Alves de Oliveira
  * @author aavls
  */
 public class MediaSemestral {
